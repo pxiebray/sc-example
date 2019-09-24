65fe4fe9ba57e3e8a5167ba465f545d7322a18a1
hyz <498621@163.com>
2019-09-24 09:27:17 +0800

    config增加native/git/jdbc配置

--- ztest-sc-business/user/src/main/java/com/ztest/sc/business/user/resources/UserResource.java
+++ ztest-sc-business/user/src/main/java/com/ztest/sc/business/user/resources/UserResource.java
@@ -1,6 +1,8 @@
 package com.ztest.sc.business.user.resources;
 
 import com.ztest.sc.business.user.vo.UserInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,19 +16,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserResource {
 
-    @Value("${hello}")
+    private static final Logger logger = LoggerFactory.getLogger(UserResource.class);
+    @Value("${hello:default}")
     private String helloValue;
 
     @Value("${server.port}")
     private String instancePort;
 
     @GetMapping
-    public String home(@PathVariable Long id) {
-        return "hello " + helloValue;
+    public String home() {
+        logger.info("id");
+            return "hello " + helloValue;
     }
 
     @GetMapping("/users/{id}")
     public UserInfo getUser(@PathVariable Long id) {
+        logger.info("id " + id);
         return new UserInfo(id, instancePort, "", "");
     }
 }
